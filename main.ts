007175e92d7e46ee7e03b076ac4137d9478940ea
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-12-08 20:29:48 +0000

    Update Set Correct Time & Date

--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
+// If Current Date/Time is wrong:
+// 
+// Set correct details in these blocks
+// 
+// Upload to MicroBit 
+// Press A+B to set new time & date
+// 
 input.onButtonPressed(Button.AB, function () {
     makerbit.clearLcd2004()
     makerbit.showStringOnLcd2004("Setting Date & Time", makerbit.position2004(LcdPosition2004.Pos1), 20, TextOption.AlignCenter)
     basic.pause(2000)
     makerbit.showStringOnLcd2004("Please Wait", makerbit.position2004(LcdPosition2004.Pos21), 20, TextOption.AlignCenter)
+    // If Current Date/Time is wrong:
+    // 
+    // Set correct details in these blocks
+    // 
+    // Upload to MicroBit 
+    // Press A+B to set new time & date
+    // 
     ds.DateTime(
     2021,
     12,
     8,
     3,
     20,
-    15,
-    0
+    25,
+    30
     )
     basic.pause(2000)
     makerbit.showStringOnLcd2004("Time Set", makerbit.position2004(LcdPosition2004.Pos21), 20, TextOption.AlignCenter)
@@ -24,6 +38,9 @@ ds.start()
 makerbit.connectLcd(39)
 makerbit.clearLcd2004()
 makerbit.showStringOnLcd2004("Current UK Time", makerbit.position2004(LcdPosition2004.Pos1), 20, TextOption.AlignCenter)
+makerbit.showStringOnLcd2004("Press A+B If Wrong!!", makerbit.position2004(LcdPosition2004.Pos61), 20, TextOption.AlignCenter)
+basic.pause(5000)
+makerbit.showStringOnLcd2004("                    ", makerbit.position2004(LcdPosition2004.Pos61), 20, TextOption.AlignCenter)
 basic.forever(function () {
     makerbit.showStringOnLcd2004("" + ds.getHour() + " : " + ds.getMinute() + " : " + ds.getSecond(), makerbit.position2004(LcdPosition2004.Pos41), 20, TextOption.AlignCenter)
 })
