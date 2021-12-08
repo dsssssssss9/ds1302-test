7c5f712ff65d0b78df9a2b418ca957c3ff6ab766
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-12-08 20:16:54 +0000

    Add set time on A+B pushed

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.AB, function () {
     makerbit.clearLcd2004()
+    makerbit.showStringOnLcd2004("Setting Date & Time", makerbit.position2004(LcdPosition2004.Pos1), 20, TextOption.AlignCenter)
+    basic.pause(2000)
+    makerbit.showStringOnLcd2004("Please Wait", makerbit.position2004(LcdPosition2004.Pos21), 20, TextOption.AlignCenter)
+    ds.DateTime(
+    2021,
+    12,
+    8,
+    3,
+    20,
+    15,
+    0
+    )
+    basic.pause(2000)
+    makerbit.showStringOnLcd2004("Time Set", makerbit.position2004(LcdPosition2004.Pos21), 20, TextOption.AlignCenter)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    control.reset()
 })
-let ds = DS1302.create(DigitalPin.P2, DigitalPin.P16, DigitalPin.P14)
+let ds: DS1302.DS1302RTC = null
+ds = DS1302.create(DigitalPin.P2, DigitalPin.P16, DigitalPin.P14)
 ds.start()
 makerbit.connectLcd(39)
 makerbit.clearLcd2004()
